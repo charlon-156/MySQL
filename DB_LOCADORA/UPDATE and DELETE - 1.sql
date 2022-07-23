ec44756c27504459f4b9bd9f0c557ea4e1ee9e5f
Charlon_F <65186965+charlon-156@users.noreply.github.com>
2022-07-23 19:37:45 -0300

    Update and Delete from Locadora

--- DB_LOCADORA/UPDATE and DELETE - 1.sql
+++ DB_LOCADORA/UPDATE and DELETE - 1.sql
@@ -6,7 +6,7 @@ use db_locadora;
 -- -- QUESTÃO 01 -- --
 
 update tb_filmes set fil_cat_codigo = 1 where fil_ano > 2012;
-update tb_filmes set fil_cat_codigo = 2 where fil_ano between 2019 and 2014;
+update tb_filmes set fil_cat_codigo = 2 where fil_ano between 2019 and 2016;
 update tb_filmes set fil_cat_codigo = 3 where fil_ano < 2019;
 
 -- -- QUESTÃO 02 -- --
@@ -15,13 +15,16 @@ insert into tb_generos (gen_genero) values ('Ação e Aventura');
 update tb_filmes set fil_gen_codigo = 11 where fil_gen_codigo in (2, 3);
 delete from tb_generos where gen_codigo in (2, 3);
 
---  -- QUESTÃO 03 -- -- 
+-- -- QUESTÃO 03 -- -- 
 
 update tb_copias set cop_con_codigo = 3 where (cop_tip_codigo = 1 or cop_tip_codigo = 3) and cop_con_codigo = 2;
 
 -- -- QUESTÃO 04 -- --  
 
-update tb_clientes set cli_nome = 'Diana Silva' where cli_nome = 'Cephes Silva';
+update tb_depdoscli set ddc_cli_codigo = 6 where ddc_cli_codigo 5;
+update tb_teldoscli set tdc_cli_codigo = 6 where tdc_cli_codigo 5;
+update tb_locacoes set loc_cli_codigo = 6 where loc_cli_codigo 5;
+delete from tb_clientes where cli_codigo = 5;
 
 -- -- QUESTÃO 05 -- --  
 

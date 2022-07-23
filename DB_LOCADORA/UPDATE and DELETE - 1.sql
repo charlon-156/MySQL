b358b782875d9e4435dd42ebbe7bbccf55ab6f36
Charlon Fernandes <charlon135@gmail.com>
2022-07-23 17:35:56 -0300

    Update and Delete from Locadora

--- DB_LOCADORA/UPDATE and DELETE - 1.sql
+++ DB_LOCADORA/UPDATE and DELETE - 1.sql
@@ -3,27 +3,44 @@
 
 use db_locadora;
 
--- Questão 01
+-- -- QUESTÃO 01 -- --
+
 update tb_filmes set fil_cat_codigo = 1 where fil_ano > 2012;
 update tb_filmes set fil_cat_codigo = 2 where fil_ano between 2019 and 2014;
 update tb_filmes set fil_cat_codigo = 3 where fil_ano < 2019;
 
--- 	Questão 02
+-- -- QUESTÃO 02 -- --
+
 insert into tb_generos (gen_genero) values ('Ação e Aventura');
 update tb_filmes set fil_gen_codigo = 11 where fil_gen_codigo in (2, 3);
 delete from tb_generos where gen_codigo in (2, 3);
 
--- 	Questão 03
+--  -- QUESTÃO 03 -- -- 
+
 update tb_copias set cop_con_codigo = 3 where (cop_tip_codigo = 1 or cop_tip_codigo = 3) and cop_con_codigo = 2;
 
--- Questão 04
+-- -- QUESTÃO 04 -- --  
+
 update tb_clientes set cli_nome = 'Diana Silva' where cli_nome = 'Cephes Silva';
 
--- Questão 05
+-- -- QUESTÃO 05 -- --  
+
 update tb_locacoes set loc_datadevolucao = 2021-04-26 where loc_datalocacao between 2021-04-22 and 2021-04-25;
 
--- Questão 06
+-- --  QUESTÃO 06 -- --  
+
+	-- Select para descobrir cid_codigo que não possui bairros
+
+	select tb_bairros.bai_bairro, tb_cidades.cid_cidade, tb_cidades.cid_codigo
+	from tb_bairros right join tb_cidades on tb_bairros.bai_cid_codigo = tb_cidades.cid_codigo;
+
 delete from tb_cidades where cid_codigo between 4 and 8;
 
--- Questão 07 
+-- -- QUESTÃO 07 -- -- 
+
+	-- Select para descobrir quais dependentes não possuem responsavel
+	select tb_dependentes.dep_nome, tb_dependentes.dep_codigo, tb_depdoscli.ddc_cli_codigo from tb_dependentes
+    left join tb_depdoscli on tb_dependentes.dep_codigo = tb_depdoscli.ddc_dep_codigo
+	left join tb_clientes on tb_depdoscli.ddc_cli_codigo = tb_clientes.cli_codigo order by tb_dependentes.dep_codigo;
+
 delete from tb_dependentes where dep_codigo in (11, 12);

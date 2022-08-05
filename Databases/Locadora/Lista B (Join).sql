ff6b2a051fb71ef2eaf58c3b17427434bb0b64f8
Charlon Fernandes <charlon135@gmail.com>
2022-08-05 14:28:40 -0300

    list b

--- Databases/Locadora/Lista B (Join).sql
+++ Databases/Locadora/Lista B (Join).sql
@@ -3,3 +3,5 @@
 select fil_titulo, gen_genero, cat_categoria from tb_filmes join tb_generos on gen_codigo = fil_gen_codigo join tb_categorias on cat_codigo = fil_cat_codigo
 where fil_gen_codigo = 9 and fil_cat_codigo = 3;
 
+-- Questão 2
+select cop_codigo, fil_titulo from tb_copias join tb_filmes on cop_fil_codigo = fil_codigo order by fil_gen_codigo
